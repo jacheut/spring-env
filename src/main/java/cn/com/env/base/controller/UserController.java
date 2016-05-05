29fd7a9d992db74edb0d2b25eb9894a0e85c75c6
jacheut <1247729252@qq.com>
2016-05-05 15:17:23 +0800

    add log and log4jdbc file

--- src/main/java/cn/com/env/base/controller/UserController.java
+++ src/main/java/cn/com/env/base/controller/UserController.java
@@ -2,6 +2,8 @@ package cn.com.env.base.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,8 @@ import cn.com.env.base.model.User;
 @Controller
 @RequestMapping("user")
 public class UserController {
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	
 	@Autowired
 	private IUserBusiness userBusiness;
 	
@@ -23,6 +27,7 @@ public class UserController {
 		User user = userBusiness.getUserById(1);
 		System.out.println("name=" + user.getUsername());
 		request.setAttribute("user", user);
+		log.info(user.getUsername());
 		return "user";
 	}
 }
